0aa1b51ea4f009e43e0f565af802cd0e348b5251
Justin de Ruiter <justin@jdr-boost.nl>
2024-06-03 16:00:18 +0200

    wip(udm): added udm package for types

--- packages/api-client/src/api/getProduct/index.ts
+++ packages/api-client/src/api/getProduct/index.ts
@@ -2,6 +2,7 @@ import consola from "consola";
 import { BoilerplateIntegrationContext } from "../../types";
 import { GetProductParams } from "./types";
 import { Product } from "src/types/models/Product";
+import { SfProduct } from "@vue-storefront/unified-data-model";
 
 export const getProduct = async (
     context: BoilerplateIntegrationContext,
@@ -27,6 +28,23 @@ export const getProduct = async (
     try {
         const response = await context.client.get<Product>(url, { headers });
         consola.info("Product fetched successfully:", response.data);
+        // Convert prodcut to SF model
+        const sfProduct: SfProduct = {
+            id: response.data.code,
+            name: response.data.name,
+            slug: response.data.slug,
+            description: response.data.description,
+            sku: response.data.code,
+            price: response.data.variants[0].,
+            images: response.data.images,
+            categories: response.data.productTaxons,
+            attributes: response.data.attributes,
+            options: response.data.options,
+            variants: response.data.variants,
+            createdAt: response.data.createdAt,
+            updatedAt: response.data.updatedAt,
+        };
+
         return response.data;
     } catch (error) {
         consola.error("Failed to fetch product:", error);
